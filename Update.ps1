c0dc144347378353292e5354352988f0da9efeb1
Read <decimation001@gmail.com>
2021-12-04 19:42:44 -0600

    update

--- Update.ps1
+++ Update.ps1
@@ -2,4 +2,4 @@ $s = "$env:APPDATA\BetterDiscord\data\stable"
 
 $cd = Get-Location
 
-Copy-Item -Path "$s\custom.css" -Destination $cd
+Copy-Item -Path "$s\custom.css" -Destination "$cd\Discord.css"
