5d1bda8bfcbdbcf7ca91f91360af2c5b341c4c33
Read Stanton <decimation001@gmail.com>
2024-08-21 17:05:29 -0500

    Update

--- Update.ps1
+++ Update.ps1
@@ -1,5 +1,7 @@
-$s = "$env:APPDATA\BetterDiscord\data\stable"
+$s = "$env:APPDATA\BetterDiscord\data\stable\"
+$s2 = "$env:APPDATA\Vencord\settings\"
 
 $cd = Get-Location
 
 Copy-Item -Path "$s\custom.css" -Destination "$cd\Discord.css"
+Copy-Item -Path "$s2\quickCss.css" -Destination "$cd\Vencord.css"
